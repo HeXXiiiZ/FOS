01ffe5e3c70575492a9dc7b5c8322399b11fcb1a
HeXXiiiZ <HeXXiiiZ@gmail.com>
2013-02-18 02:57:46 -0800

    Changes: turn and face the strange.

--- js/FOS-Manifesto.js
+++ js/FOS-Manifesto.js
@@ -2,16 +2,13 @@
 
 
 var main = function() {
-    $('article').click(function() {
-        var that = $(this)
-        that.children('section').toggle();
-        that.children('aside').toggle();
-        that.children('footer').toggle();
-    });
-
-    $('article').children('section').hide();
-    $('article').children('aside').hide();
-    $('article').children('footer').hide();
+    var headers = $('article > header');
+    headers.click(function() { $(this).siblings().toggle(); });
+    headers.siblings().hide();
+    headers.hover(
+        function() { $(this).addClass("roll"); },
+        function() { $(this).removeClass("roll"); }
+    );
 };
 
 
