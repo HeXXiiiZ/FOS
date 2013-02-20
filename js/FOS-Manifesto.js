5dbd02ed0ed52974ef033b738a7daec8ff8cd068
HeXXiiiZ <HeXXiiiZ@gmail.com>
2013-02-20 11:32:23 -0800

    Added Expand/Contract buttons

--- js/FOS-Manifesto.js
+++ js/FOS-Manifesto.js
@@ -3,8 +3,15 @@
 
 var main = function() {
     var headers = $('article > header');
+
     headers.click(function() { $(this).siblings().toggle(); });
-    headers.siblings().hide();
+    var expand   = function() { headers.siblings().show(); };
+    var contract = function() { headers.siblings().hide(); };
+    contract();
+
+    $('#expander').click(expand);
+    $('#contractor').click(contract);
+
     headers.hover(
         function() { $(this).addClass("roll"); },
         function() { $(this).removeClass("roll"); }
