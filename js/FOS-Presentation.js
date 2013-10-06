68cb2092d2e116538920010652307c756f72e632
shaver <shaver@ealmac18.local>
2013-10-06 00:34:42 -0700

    Basic Infrastructure in place. Diagrams added.

--- js/FOS-Presentation.js
+++ js/FOS-Presentation.js
@@ -1,30 +1,10 @@
 
 
 
-
 var PP = require('prelude-ls');
 
 
-function getUnicodeCharacter(cp) {
-    if (cp >= 0 && cp <= 0xD7FF || cp >= 0xE000 && cp <= 0xFFFF) {
-        return String.fromCharCode(cp);
-    } else if (cp >= 0x10000 && cp <= 0x10FFFF) {
 
-        // we substract 0x10000 from cp to get a 20-bits number
-        // in the range 0..0xFFFF
-        cp -= 0x10000;
-
-        // we add 0xD800 to the number formed by the first 10 bits
-        // to give the first byte
-        var first = ((0xffc00 & cp) >> 10) + 0xD800
-
-        // we add 0xDC00 to the number formed by the low 10 bits
-        // to give the second byte
-        var second = (0x3ff & cp) + 0xDC00;
-
-        return String.fromCharCode(first) + String.fromCharCode(second);
-    }
-}
 
 
 function peopleLink(person) {
@@ -94,6 +74,12 @@ function refReplace(sel, fun) {
 }
 
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Presentation Structure Builder Code //
+////////////////////////////////////////////////////////////////////////////////
+
 function buildStructure() {
     var cont = $.deck('getContainer');
     var struct = { doc : { slides : [], subs : [], elem : cont } };
@@ -133,6 +119,10 @@ function buildStructure() {
 }
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Title Code //
+////////////////////////////////////////////////////////////////////////////////
+
 function makeSubTitle(name) {
     return $('<section />', { 'class' : 'slide' }).append('<h1>' + name + '</h1>');
 }
@@ -144,10 +134,16 @@ function addTitles(struct) {
     });
 }
 
+
+////////////////////////////////////////////////////////////////////////////////
+// Table of Contents Code //
+////////////////////////////////////////////////////////////////////////////////
+
+
 function buildToc(struct, hidex) {
     var tocOl = $('<ol />');
     _.each(struct.doc.subs, function (el, dex) {
-        var tslide = el.tslide || PP.first(el.slides).elem;
+        var tslide = el.tslide || PP.empty(el.slides) ? '#' : PP.first(el.slides).elem;
         var anch = $('<a />', { 'class' : 'backlink' }).html(el.title).data('ref', tslide);
         var subbar = $('<div />', { 'class' : 'subbar' });
         _.each(el.subs, function (et) {
@@ -168,12 +164,12 @@ function buildToc(struct, hidex) {
 function buildTopbar(struct, hidex) {
     var topbar = $('<div />', {'class' : 'topbar'});
 
-    var tslide = struct.tslide || PP.first(struct.slides).elem;
+    var tslide = struct.tslide || PP.empty(struct.slides) ? '#' : PP.first(struct.slides).elem;
     var anch = $('<a />', { 'class' : 'backlink' }).html(struct.title).data('ref', tslide);
     topbar.append($('<span />', { 'class' : 'topbarHead' }).append(anch));
 
     _.each(struct.subs, function (el, dex) {
-        var fslide = PP.first(el.slides).elem;
+        var fslide = PP.empty(el.slides) ? '#' : PP.first(el.slides).elem;
         var subanch = $('<a />', { 'class' : 'backlink' }).html(el.title).data('ref', fslide);
         var cell = $('<span />').append(subanch);
         cell.addClass(hidex == dex ? 'topbarSubHi' : 'topbarSub');
@@ -182,6 +178,11 @@ function buildTopbar(struct, hidex) {
 
     return topbar;
 }
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Reference Code //
+////////////////////////////////////////////////////////////////////////////////
 
 function resolveReferences() {
     $("span.ref").each(function() {
@@ -201,6 +202,7 @@ function resolveReferences() {
     refReplace("span.btitle", function(ref) {
         return "<b>" + ref.title + "</b>";
     });
+
 }
 
 function buildBiblio() {
@@ -211,6 +213,12 @@ function buildBiblio() {
 
     var bibs = _.keys(bibsSet);
 }
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Setup Code //
+////////////////////////////////////////////////////////////////////////////////
 
 $(document).bind('deck.beforeInit', function() {
     buildStructure();
@@ -223,7 +231,8 @@ $(document).bind('deck.beforeInit', function() {
     _.each(struct.doc.subs, function (el, dex) {
         var toc = buildToc(struct, dex);
         if (el.tslide) el.tslide.append(toc);
-        else PP.first(el.slides).elem.prepend(toc)
+        else if (!PP.empty(el.slides)) PP.first(el.slides).elem.prepend(toc);
+        else ;
         _.each(el.slides, function (et) { et.elem.prepend(buildTopbar(el)); });
         _.each(el.subs, function (et, ddex) {
             _.each(et.slides, function(en) { en.elem.prepend(buildTopbar(el, ddex)); });
@@ -239,7 +248,8 @@ $(document).bind('deck.beforeInit', function() {
 $(document).bind('deck.init', function() {
     $('a.backlink').each(function(dex, el) {
         var ell = $(el);
-        ell.attr('href', '#' + ell.data('ref').attr('id'));
+        var dref = ell.data('ref');
+        ell.attr('href', '#' + (dref === '#' ? '' : dref.attr('id')));
     });
 });
 
@@ -247,6 +257,109 @@ $(document).bind('deck.init', function() {
 PP.last(str.doc.slides).elem
 .after($('<section />', { 'class': 'slide' }).append('<h2>New Slide</h2>'))
  */
+
+
+function getElementCenter(elem) {
+  var bb = elem.getBBox();
+  return {
+    'x': bb.x + 0.5*bb.width, 
+    'y': bb.y + 0.5*bb.height  
+  };
+}
+
+
+function drawing() {
+  var paper = Raphael('sci-model-1', 550, 670);
+  var sciModel = makeSciModel(paper);
+  var communlets = makeClassicCom(paper, sciModel);  
+}
+
+function makeClassicCom(paper, model) {
+  makeCom(paper, model, 'Proposal', "Funding");
+  makeCom(paper, model, 'Writing', "Peer-Review");
+  makeCom(paper, model, 'Publication', "Popular Media");
+}
+
+
+function makeCom(paper, model, key, comLabel) {
+  var cboxHeight = 35;
+  var cboxWidth  = 120;
+  var cboxDist   = 150;
+  var cboxHeightH = 0.5*cboxHeight;
+
+  var cjoin = model.arrows[model.stageIndex[key]].center;
+  cjoin.y -= 3;
+  var cnode = paper.rect(cjoin.x + cboxDist, cjoin.y - cboxHeightH, cboxWidth, cboxHeight, 5);
+  cnode.attr({ 'fill': '#ff0000' });
+
+  var cent = getElementCenter(cnode); 
+  var label = paper.text(cent.x, cent.y, comLabel);
+  label.attr({
+    'font-size' : '16pt',
+    'fill'      : '#8fff8f',
+  });
+
+  var carr  = paper.path([ 'M', cjoin.x - 70, cjoin.y, 'L', cjoin.x + cboxDist - 10, cjoin.y ]);
+  carr.attr({
+    'stroke'       : '#ff1111',
+    'stroke-width' : '2px',
+    'stroke-linecap' : 'round'
+  });
+
+}
+
+function makeSciModel(paper) {
+  var boxHeight = 50;
+  var boxWidth  = 150;
+  var boxGap    = 30;
+  var boxXpos   = 10;
+  var stages = [
+    'Background', 'Synthesis', 'Proposal', 'Experiment',
+    'Analysis', 'Writing', 'Publication', 'Dissemination'
+  ];
+
+  var pos = 10;
+  var stageNodes = PP.map(function(el) {
+    var rect = paper.rect(boxXpos, pos, boxWidth, boxHeight, 10);
+    rect.attr({ 'fill': '#10008f' });
+
+    var cent = getElementCenter(rect); 
+    var label = paper.text(cent.x, cent.y, el);
+    label.attr({
+      'font-size' : '16pt',
+      'fill'      : '#8fff8f',
+    });
+
+    pos += boxHeight + boxGap;
+    return { 'label': el, 'node': rect, 'center': cent };  
+  }, stages);
+
+  var stageArrows = PP.map(function(el) {
+    var boxHeightH = boxHeight/2;
+    var cent = el.center;
+    var ays = cent.y - boxGap - boxHeightH;
+    var aye = cent.y - boxHeightH - 6;
+    var aym = ays + 0.5*(aye - ays);
+    var acent = { 'x': cent.x, 'y': aym };
+    var arr  = paper.path([ 'M', cent.x, ays, 'L', cent.x, aye ]);
+    arr.attr({
+      'stroke'       : '#ffffff',
+      'stroke-width' : '5px',
+      'arrow-end'    : 'classic-medium-medium'
+    });
+
+    return { 'arr': arr, 'center': acent };
+  }, PP.tail(stageNodes));
+
+  var stageIndex = {}, k = 0;
+  PP.each(function(el) { stageIndex[el] = k; k += 1; }, stages);
+
+  return { 'stageIndex': stageIndex, 'nodes': stageNodes, 'arrows': stageArrows };
+}
+
+
+
+
 
 
 
